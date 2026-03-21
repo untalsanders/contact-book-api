147349798206c210d0d87835d67850f0f408f3d8
Sanders Gutiérrez <ing.sanders@gmail.com>
2026-03-21 01:44:18 -0300

    refactor(app): rename ContactsApiApplication to ContactBookApiApplication
    Rename the main Spring Boot application class and its corresponding file from ContactsApiApplication to ContactBookApiApplication.

--- src/main/java/com/untalsanders/contacts/ContactBookApiApplication.java
+++ src/main/java/com/untalsanders/contacts/ContactBookApiApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactsApiApplication {
+public class ContactBookApiApplication {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(ContactsApiApplication.class);
+        SpringApplication application = new SpringApplication(ContactBookApiApplication.class);
         application.setBannerMode(Banner.Mode.OFF);
         application.run(args);
     }
